e23e6bdc9075cb2318b96dae6fa5d8917cdbe840
mmahesh7 <mmahesh23022@gmail.com>
2025-08-04 22:27:04 +0530

    Exception Handling, Enums, Object Cloning & More

---
+++ OOP_7/Exception_Handling/Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String[] args) {
+        
+        Main.divide(5, 0); // This will throw an ArithmeticException
+        // Displays the message from the exception of Main class
+        // i.e, "Please do not divide by zero"
+    }
+}
